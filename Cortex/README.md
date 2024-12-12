5ecd225e140e69e8d43114ffc7b2d4b795ed8202
CamH04 <cameronahaynes04@gmail.com>
2024-12-12 09:34:34 +0000

    printf imp does compile

---
+++ Cortex/README.md
@@ -0,0 +1,7 @@
+#Resources 
+
+##Arm-CA15 Docs
+https://developer.arm.com/documentation/#numberOfResults=48&cfnavigationhierarchiesproducts=IP%20Products,Processors,Cortex-A,Cortex-A15
+
+##OsDev Article
+https://wiki.osdev.org/ARMv7-A_Bare_Bones
--- Cortex/printf.c
+++ Cortex/printf.c
@@ -1,38 +1,8 @@
-#include <stdint.h>
-//base address of the first UART
-#define UART0_BASE 0x1c090000
-
-//this is for the ARMv7-A (32 bit) Cortex-A15
-
-void printAlbaOsForArm(){
-    *(volatile uint32_t *)(UART0_BASE) = 'A';
-    *(volatile uint32_t *)(UART0_BASE) = 'L';
-    *(volatile uint32_t *)(UART0_BASE) = 'B';
-    *(volatile uint32_t *)(UART0_BASE) = 'A';
-    *(volatile uint32_t *)(UART0_BASE) = 'O';
-    *(volatile uint32_t *)(UART0_BASE) = 'S';
-    *(volatile uint32_t *)(UART0_BASE) = ' ';
-    *(volatile uint32_t *)(UART0_BASE) = 'F';
-    *(volatile uint32_t *)(UART0_BASE) = 'O';
-    *(volatile uint32_t *)(UART0_BASE) = 'R';
-    *(volatile uint32_t *)(UART0_BASE) = ' ';
-    *(volatile uint32_t *)(UART0_BASE) = 'A';
-    *(volatile uint32_t *)(UART0_BASE) = 'R';
-    *(volatile uint32_t *)(UART0_BASE) = 'M';
-    *(volatile uint32_t *)(UART0_BASE) = '\n';
-}
-
-void start() {
-    printAlbaOsForArm();
-    
-}
-
-
 #include <stdarg.h>
+#include <stdint.h>
 
 // Define memory-mapped I/O address for UART0, used for printing
-
-//#define UART0_BASE 0x3F201000
+#define UART0_BASE 0x3F201000
 #define UART0_DR   *(volatile uint32_t*)(UART0_BASE + 0x00)
 #define UART0_FR   *(volatile uint32_t*)(UART0_BASE + 0x18)
 
@@ -105,9 +75,10 @@ void uart_printf(const char* format, ...) {
     va_end(args);
 }
 
-void testprintf(){
-        uart_printf("Hello, World!\n");
-        uart_printf("Integer: %d\n", 12345);
-        uart_printf("String: %s\n", "Test string");
+int main() {
+    uart_printf("Hello, World!\n");
+    uart_printf("Integer: %d\n", 12345);
+    uart_printf("String: %s\n", "Test string");
 
+    return 0;
 }
